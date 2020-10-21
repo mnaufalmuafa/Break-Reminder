df43d078968d4a832ec8f36875abfc316b987141
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-10-21 15:52:00 +0700

    Memperbaiki visibilitas keterangan

--- app/src/main/java/com/example/breakreminder/MainActivity.java
+++ app/src/main/java/com/example/breakreminder/MainActivity.java
@@ -19,18 +19,22 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private TextView keterangan;
+    private String timer = "0";
+    private boolean first = true;
+    private int k = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        keterangan = findViewById(R.id.keterangan);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
-        keterangan = findViewById(R.id.keterangan);
 
     }
 
@@ -45,28 +49,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_LONG;
         Toast toast;
-
-        if (position == 0) {
-            keterangan.setText("You will be reminded in 30 minutes");
-            toast = Toast.makeText(context, "You will be reminded in 30 minutes", duration);
-        }
-        else if (position == 1) {
-            keterangan.setText("You will be reminded in 1 hour");
-            toast = Toast.makeText(context, "You will be reminded in 1 hour", duration);
-        }
-        else if (position == 2) {
-            keterangan.setText("You will be reminded in 1 hour 30 minutes");
-            toast = Toast.makeText(context, "You will be reminded in 1 hour 30 minutes", duration);
+        if (k < 2) {
+            k = 2;
         }
         else {
-            keterangan.setText("You will be reminded in 2 hour");
-            toast = Toast.makeText(context, "You will be reminded in 2 hour", duration);
+            k = 2;
+            if (position == 0) {
+                setTimer("30 minutes");
+                toast = Toast.makeText(context, "You will be reminded in 30 minutes", duration);
+            }
+            else if (position == 1) {
+                setTimer("1 hour");
+                toast = Toast.makeText(context, "You will be reminded in 1 hour", duration);
+            }
+            else if (position == 2) {
+                setTimer("1 hour 30 minutes");
+                toast = Toast.makeText(context, "You will be reminded in 1 hour 30 minutes", duration);
+            }
+            else {
+                setTimer("2 hour");
+                toast = Toast.makeText(context, "You will be reminded in 2 hour", duration);
+            }
+            toast.show();
+            first = true;
         }
-        toast.show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    private void setTimer(String time) {
+        timer = time;
+        Log.d("DEBUGTAG", time);
+        if (time.equals("0") && first) {
+            keterangan.setVisibility(View.INVISIBLE);
+        }
+        else {
+            keterangan.setVisibility(View.VISIBLE);
+            String placeholder = "You will be reminded in " + timer;
+            keterangan.setText(placeholder);
+        }
     }
 }
